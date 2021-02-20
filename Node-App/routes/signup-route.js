c9d85567b544ff99504e3ba93cfd648ea36de96c
ltr <akambampati@hcf.com.au>
2021-02-20 14:48:21 +0530

    removed response handler in route

--- Node-App/routes/signup-route.js
+++ Node-App/routes/signup-route.js
@@ -1,20 +1,6 @@
 const routes = require("express").Router();
 const authenticateCtrl = require("../controller/signup.controller");
 
-routes.get("/signup", async (req, res, next) => {
-  try {
-    const response = await authenticateCtrl.signUpCtrl(req, res);
-    console.log("response", response);
-    res.status(201).json({
-      success: true,
-      data: response,
-    });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      data: err,
-    });
-  }
-});
+routes.get("/signup", authenticateCtrl.signUpCtrl);
 
 module.exports = routes;
