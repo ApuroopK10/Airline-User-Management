11358771c6f7c21d5ed88c11ca2f9e530977a7be
ltr <akambampati@hcf.com.au>
2021-02-20 16:52:09 +0530

    updated schema for including children keys

--- Node-App/models/user.js
+++ Node-App/models/user.js
@@ -19,6 +19,11 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
   },
+  children: [
+    {
+      type: String,
+    },
+  ],
 });
 
 module.exports = mongoose.model("User", UserSchema);
