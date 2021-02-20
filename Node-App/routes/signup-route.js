9e512de52414c891fdcd2c8e3adb657a4855a8c2
ltr <akambampati@hcf.com.au>
2021-02-20 15:23:25 +0530

    changed to post after testing

--- Node-App/routes/signup-route.js
+++ Node-App/routes/signup-route.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 const authenticateCtrl = require("../controller/signup.controller");
 
-routes.get("/signup", authenticateCtrl.signUpCtrl);
+routes.post("/signup", authenticateCtrl.signUpCtrl);
 
 module.exports = routes;
