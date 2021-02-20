fe2d6b90fd2092a12ec623ef2934f5b4c641f161
ltr <akambampati@hcf.com.au>
2021-02-20 15:11:08 +0530

    signup API created

---
+++ Node-App/controller/signup.controller.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+exports.signUpCtrl = async (req, res) => {
+  try {
+    const { name, email, role, password } = req.body;
+    const response = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    res.status(201).json({
+      success: true,
+      data: response,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      data: err,
+    });
+  }
+};
