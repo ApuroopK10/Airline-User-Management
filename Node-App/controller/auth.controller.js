84eac2130eb2fe3fff342d472c3a10d90f427aab
ltr <akambampati@hcf.com.au>
2021-02-20 16:20:06 +0530

    replaced try catch with asyncHandler

--- Node-App/controller/auth.controller.js
+++ Node-App/controller/auth.controller.js
@@ -1,23 +1,17 @@
+const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
 
-exports.signUp = async (req, res) => {
-  try {
-    const { name, email, role, password } = req.body;
-    const response = await User.create({
-      name,
-      email,
-      password,
-      role,
-    });
+exports.signUp = asyncHandler(async (req, res) => {
+  const { name, email, role, password } = req.body;
+  const response = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
 
-    res.status(201).json({
-      success: true,
-      data: response,
-    });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      data: err,
-    });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: response,
+  });
+});
