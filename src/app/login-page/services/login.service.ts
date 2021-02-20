bc01e9ac385254e757dd1f25dc9c2ac90860e9c0
ltr <akambampati@hcf.com.au>
2021-02-20 22:06:02 +0530

    saving user data in shared service

--- src/app/login-page/services/login.service.ts
+++ src/app/login-page/services/login.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/shared/models/User.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,19 @@ export class LoginService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
+userData: User;
 
   constructor(private httpClient: HttpClient) { }
 
   signUp (payload) {
     return this.httpClient.post(`http://localhost:3002/authenticate/signup`, payload, this.httpOptions);
+  }
+
+  setUserData(user: User) {
+    this.userData = user;
+  }
+
+  getUserData() : User {
+    return this.userData;
   }
 }
