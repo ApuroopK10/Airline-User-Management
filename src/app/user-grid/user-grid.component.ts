9a4b592d001ff67013f5f60467f2f7a1c796d6ff
ltr <akambampati@hcf.com.au>
2021-02-23 23:00:15 +0530

    Add/Delete changes

--- src/app/user-grid/user-grid.component.ts
+++ src/app/user-grid/user-grid.component.ts
@@ -9,10 +9,9 @@ import { UserOpsService } from './services/user-ops.service';
   selector: 'app-user-grid',
   templateUrl: './user-grid.component.html',
   styleUrls: ['./user-grid.component.scss'],
-  providers:[MessageService]
+  providers: [MessageService],
 })
 export class UserGridComponent implements OnInit {
-
   allUsers = [];
   userHeaders;
   editUser: User;
@@ -21,28 +20,42 @@ export class UserGridComponent implements OnInit {
   userRoles;
   addUser = false;
   isLoading = false;
+  parentUser: User;
   @ViewChild('modalRef') private modalRef: ElementRef;
-  constructor(private opsService: UserOpsService, private loginService: LoginService,
-    private messageService: MessageService) { }
+  constructor(
+    private opsService: UserOpsService,
+    private loginService: LoginService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.userHeaders = userHeaders;
     this.userRoles = rolesNoSuperAdmin;
-    const allUsers = this.loginService.getUserData();
-    if (allUsers.children.length > 0) {
-      this.opsService.userOperations(allUsers.children, 'getAllUsers').subscribe(response => {
-        this.isLoading = false;
-      }, error => {
-        this.messageService.add({severity: 'error', summary: 'Error', detail: `Failed to get Users-${error}`});
-        this.isLoading = false;
-      });
+    const loggedInUser = this.loginService.getUserData();
+    if (loggedInUser.children.length > 0) {
+      this.opsService
+        .userOperations({ ids: loggedInUser.children }, 'getAllUsers')
+        .subscribe(
+          (response) => {
+            if (response && response['success'] && response['data']) {
+              this.allUsers = [...response['data']];
+            }
+            this.isLoading = false;
+          },
+          (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Failed to get Users-${error}`,
+            });
+            this.isLoading = false;
+          }
+        );
     } else {
       this.allUsers = [];
     }
-    
-    
-    // this.userData = userData;
 
+    // this.userData = userData;
   }
 
   updateUser(row) {
@@ -50,47 +63,76 @@ export class UserGridComponent implements OnInit {
   }
 
   deleteUser(rowIndex) {
-
     const parentUser = this.loginService.getUserData();
-    this.opsService.userOperations({id: this.allUsers[rowIndex]._id}, 'deleteUser').subscribe(response => {
-      if (response) {
-        this.allUsers.splice(rowIndex, 1);
-        this.allUsers = [...this.allUsers];
-      }
-      this.isLoading = false;
-    }, error => {
-      this.messageService.add({severity: 'error', summary: 'Error', detail: `Failed to delete Users-${error}`});
-      this.isLoading = false;
-    });
+    this.opsService
+      .userOperations(
+        { id: this.allUsers[rowIndex]._id, parentUser },
+        'deleteUser'
+      )
+      .subscribe(
+        (response) => {
+          if (response && response['success'] && response['data']) {
+            this.allUsers = [...response['data']['gridData']];
+            if (response['data']['parent']) {
+              this.loginService.setUserData(response['data']['parent']);
+            }
+          }
+          this.isLoading = false;
+        },
+        (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: `Failed to delete Users-${error}`,
+          });
+          this.isLoading = false;
+        }
+      );
   }
 
   addClicked() {
     this.addUser = true;
+    this.user = new User('', '', '', '', []);
   }
 
-  userOperations(type: string, data?) {
-    if(type === 'add') {
-      const { name, email, password, role, children} = this.user;
+  userOperations(type: string) {
+    if (type === 'add') {
+      const { name, email, password, role, children } = this.user;
       const parentUser = this.loginService.getUserData();
-      this.opsService.userOperations({newUser: {name, email, password, role: role['value'], children}, parentUser}, 'createUser').subscribe(response => {
-        if(response && response['data']) {
-          this.allUsers = [...response['data']];
-        }
-        this.isLoading = false;
-        this.addUser = false;
-        this.modalRef.nativeElement.click();
-      }, error => {
-        this.messageService.add({severity: 'error', summary: 'Error', detail: 'Failed to create Users'});
-        this.isLoading = false;
-        this.addUser = false;
-      });
+      this.opsService
+        .userOperations(
+          {
+            newUser: { name, email, password, role: role['value'], children },
+            parentUser,
+          },
+          'createUser'
+        )
+        .subscribe(
+          (response) => {
+            if (response && response['success'] && response['data']) {
+              this.allUsers = [...response['data']['gridData']];
+              if (response['data']['parent']) {
+                this.loginService.setUserData(response['data']['parent']);
+              }
+            }
+            this.isLoading = false;
+            this.addUser = false;
+            this.modalRef.nativeElement.click();
+          },
+          (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Failed to create Users - ${error}`,
+            });
+            this.isLoading = false;
+            this.addUser = false;
+          }
+        );
     }
-    
   }
 
   closeModal() {
     this.modalRef.nativeElement.click();
-
   }
-
 }
